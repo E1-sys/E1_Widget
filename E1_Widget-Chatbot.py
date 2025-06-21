cd2621b1324f3a90a04c20bc469248efc5add62a
E1-sys <brian051@g.skku.edu>
2025-06-21 13:06:29 +0900

    Update E1_Widget-Chatbot.py

--- E1_Widget-Chatbot.py
+++ E1_Widget-Chatbot.py
@@ -15,7 +15,7 @@ from urllib.parse import urljoin, urlparse
 import ssl
 import urllib3
 from selenium import webdriver
-from selenium.webdriver.common.by import
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
